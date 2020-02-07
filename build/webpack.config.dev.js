57375f044ee5f8c0e5bebf792c7077f13c09020a
nicholaschun <nicholaschunrayne@gmail.com>
2020-02-07 23:47:58 +0000

    formatted webpack config

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,42 +1,42 @@
-const { join } = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
-const { HotModuleReplacementPlugin } = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { join } = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const { HotModuleReplacementPlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: [join(__dirname, "./../src/js/main.js")],
+  mode: 'development',
+  entry: [join(__dirname, './../src/js/main.js')],
   devServer: {
     port: 3000,
     hot: true,
     open: true,
     historyApiFallback: true,
     compress: true,
-    contentBase: join(__dirname, "./../src/")
+    contentBase: join(__dirname, './../src/')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader"
+        loader: 'vue-loader'
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", MiniCssExtractPlugin.loader, "css-loader"]
+        use: ['vue-style-loader', MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "img/",
-              publicPath: "img/",
+              outputPath: 'img/',
+              publicPath: 'img/',
               esModule: false
             }
           }
@@ -45,16 +45,16 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            outputPath: "fonts/",
-            publicPath: "fonts/"
+            outputPath: 'fonts/',
+            publicPath: 'fonts/'
           }
         }
       },
       {
         test: /\.html$/i,
-        loader: "html-loader"
+        loader: 'html-loader'
       }
     ]
   },
@@ -62,8 +62,8 @@ module.exports = {
     new HotModuleReplacementPlugin(),
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/index.html"
+      template: 'src/index.html'
     }),
     new MiniCssExtractPlugin()
   ]
-};
+}
